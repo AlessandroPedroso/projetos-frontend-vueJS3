c3e7f25533d57a9b941b66124c6788b3e9c1ff8d
Your Name <alessanrospedroso@outlook.com>
2024-08-20 15:10:56 -0300

    criação de componente com script setup

---
+++ Capitulo 1/Exercise1.01/src/components/Exercise1-01.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>My firts components!</h1>
+        <span>{{ color }}</span>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            color:'red'
+        }
+    }
+}
+
+</script>
+
+<style>
+h1 {
+    font-family: 'Avenir', Arial, Helvetica, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
