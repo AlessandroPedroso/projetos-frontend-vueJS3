06bcc7317fc64757a703b1919990e02118619397
Your Name <alessanrospedroso@outlook.com>
2024-08-27 08:35:48 -0300

    criação de componentes com script setup

---
+++ Capitulo 1/Exercise1.01/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <Logo/>
+    </header>
+    <div>
+        {{ color }}
+    </div>
+</template>
+
+<script setup>
+import Logo from '../components/Logo.vue'
+const color = 'red'
+
+</script>
